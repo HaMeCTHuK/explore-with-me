3a068c3da91d72ef0444319beed4cf0197610ee4
Qweros <luckyvicar@yandex.ru>
2024-07-08 11:59:31 +0300

    Add schema, add user

---
+++ stats-server/stat-client/src/main/java/ru/practicum/client/common/DateFormatter.java
@@ -0,0 +1,7 @@
+package ru.practicum.client.common;
+
+import java.time.format.DateTimeFormatter;
+
+public class DateFormatter {
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+}
